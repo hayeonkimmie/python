33f39a7fbba741efb2a30d3e6720f70b5234b40e
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-05 23:50:25 +0900

    Add files via upload
    hello python

---
+++ hello.py
@@ -0,0 +1,29 @@
+# print('Good morning!')
+# print('Good morning!')
+# print('Good morning!')
+# print('Good night')
+# print('Good night')
+# print('Good night')
+# print('hong gil dong.')
+# print('20살')
+# print('korea seoul ~~~')
+# print('abc@gmail.com')
+#
+# print('python study!!')
+# print('도서 구입 : 20,000원')
+# print('python study')
+# print('친구야 안녕')
+#
+#
+# import math
+# print('2의 제곱근 %.2f' % math.sqrt(2))
+#
+# data+=10 #data라는 변수에 data+10 값을 할당한다.
+
+
+a=True
+b=False
+
+print((a or b) and a)
+#operator.floordiv = 몫 구하기 기능
+#continue 를 만나면 다음 실행을 생략하고, 다음 반복 실행문으로 넘어간다.
