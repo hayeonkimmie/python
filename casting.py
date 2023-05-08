15e222c7bfecc8f3a259bfe498ff99ba80d67594
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-08 18:10:46 +0900

    casting_2_007

---
+++ casting.py
@@ -0,0 +1,31 @@
+iNum=10
+fNum=3.14
+
+print(iNum)
+print(type(iNum))
+
+print(fNum)
+print(type(fNum))
+
+iNum=str(iNum)
+print(iNum)
+print(type(iNum))
+
+fNum = str(fNum)
+print(fNum)
+print(type(fNum))
+
+
+flag=True
+flag=str(flag)
+print(flag)
+print(type(flag))
+
+fNum1=3.14
+fNum2=0.123
+
+print(fNum1 + fNum2)
+fNum1=str(fNum1)
+fNum2=str(fNum2)
+
+print(fNum1+fNum2)
