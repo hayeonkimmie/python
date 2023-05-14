e6d44d7e6a62df2762807ae7707eef2d2d95c0e5
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-14 21:12:25 +0900

    condition_2_024

---
+++ condition.py
@@ -0,0 +1,41 @@
+# passScore = 80
+# myScore = int(input('점수 입력 :'))
+# # if myScore >=passScore :
+# #     print('PASS!!')
+# #
+# # if myScore < passScore :
+# #     print('FAIL!!')
+# #
+# # if myScore >=passScore:
+# #     print('PASS!!')
+# #
+# # else:
+# #     print('FAIL!!')
+# #
+# # if myScore <passScore: #pass는 나중에 코딩하겠다라는 뜻으로 오류 안내고 보류해두려면 pass라 적어두기
+# #     pass
+# #
+# # else:
+# #     pass
+# #
+# # print(len('가나다'))
+# #
+# # if len(messageString)>=500 :
+# #     ass
+# # else:
+# #     pass
+#
+#
+# floatNum = float(input('소수 입력:'))
+#
+# if floatNum - int(floatNum)>= 0.5:
+#     print('올림 : {}'.format(int(floatNum)+1))
+# else:
+#     print('버림:{}'.format(int(floatNum)))
+
+seniorAge = 65
+passengerAge =int(input('나이 입력:'))
+if passengerAge >= seniorAge:
+    print('무료 대상 승객입니다.')
+else:
+    print('유료 대상 승객입니다.')
