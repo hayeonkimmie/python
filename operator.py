194c64cd1459a891d51850d0a3487d12bcf3e7ba
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-14 21:11:55 +0900

    operator_2_023

--- operator.py
+++ operator.py
@@ -1,46 +1,33 @@
-# # num1=10
-# # num2=20
-# # result=num1+num2
-# #
-# # print(result)
-# #
-# # print('num: {}'.format(n+=3))
-# num1=10
-# num1 += 3 #num1 = num1+3
-# print('num :{}'.format(num1))
+# if 10 > 5 : #이 조건문이 트루인 경우에, 실행함.
+#     print ('10은 5보다 크다.')
+#     print('또 다른 실행문 !!')
+#
+# print('조건문이 끝났어요,')
 #
 #
-# num1=10
-# num1 /= 3 #num1 = num1+3
-# print('num :{}'.format(num1))
-# #나누면 항상 float type
+# num1= 10
+# num2 = 20
 #
-# num=10
-# num//=3
-# print('num:{}'.format(num))
-#
-# num=10
-# num**=3
-# print('num:{}'.format(num))
+# if num1 == num2:
+#     print('num1<=num2') #거짓이니까 실행이 안 됨.
+
+korScore = int(input('국어 점수:'))
+matScore = int(input('수학 점수:'))
+engScore = int(input('영어 점수:'))
 
 
-rainAmount = 0
-totalRainAmount=0
+avgScore = (korScore + matScore + engScore) /3
+print('평균 : {}'.format(avgScore))
+if avgScore >= 90:
+    print('참 잘했어요~')
 
-totalRainAmount += 30
-print('1월 누적 강수량: {}mm'.format(totalRainAmount))
 
-totalRainAmount += 45
-print('2월 누적 강수량: {}mm'.format(totalRainAmount))
+highTemperature = 28
+lowTemperature = 20
 
-totalRainAmount += 122
-print('3월 누적 강수량: {}mm'.format(totalRainAmount))
+innerTemperature = int(input('실내 온도 입력:'))
+if innerTemperature >= highTemperature:
+    print('냉방 작동!')
 
-totalRainAmount += 177
-print('4월 누적 강수량: {}mm'.format(totalRainAmount))
-
-print('-' *30)
-print('연간 누적 강수량:{}'.format(totalRainAmount))
-print('-'*30)
-
-print('연간 평균 강수량: {}mm'.format(totalRainAmount/rainAvgAmount))
+if innerTemperature <= lowTemperature:
+    print('난방 작동!')
