4db5d1e8b142b7f8b9130aece351802775a3f909
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-15 21:56:19 +0900

    condition_2_026

--- condition.py
+++ condition.py
@@ -1,20 +1,41 @@
-# rainPercentage = int(input('비율 확률 입력:'))
-# minRainPercentage = 55
+# exampleScore = int(input('시험 성적 입력:'))
+# grades =''
 #
-# print('우산 챙기세요.') if rainPercentage >= minRainPercentage else print('양산 챙기세요')
-#
-# if rainPercentage >=minRainPercentage:
-#     print('우산 챙기세요')
+# if exampleScore >= 90:
+#     grades ='A'
+# elif exampleScore >=80:
+#     grades='B'
+# elif exampleScore >=70 :
+#     grades='C'
+# elif exampleScore >=60:
+#     grades='D'
 # else:
-#     print('양산 챙기세요')
+#     grades='F'
+#
+# print('성적 :{}\t 학점:{}'.format(exampleScore, grades))
 
 
+print('1. 카페라떼 (3.5) \t 2. 에스프레소(3.0) \t 3. 아메리카노 (2.0) \t 4. 곡물라떼 (4.0) \t 5. 밀크티(4.3)')
+userSelectNumber = int(input('메뉴 선택:'))
 
-lowTemp = int(input('최저 기온 입력:'))
-highTemp = int(input('최고 기온 입력:'))
+print ('-' * 43)
+if userSelectNumber ==1:
+    print('메뉴 : 카페라떼')
+    print('가격 : 3,500원')
 
-tempDifference = highTemp - lowTemp
-if tempDifference>=11:
-    print('감기 조심하세요')
-else:
-    print('산책하기 좋은 날씨 입니다.')
+elif userSelectNumber == 2:
+    print('메뉴 : 에스프레소')
+    print('가격 : 3,000원')
+
+elif userSelectNumber == 3:
+    print('메뉴 : 아메리카노')
+    print('가격 : 2,000원')
+
+elif userSelectNumber == 4:
+    print('메뉴 : 곡물라떼')
+    print('가격 : 4,000원')
+
+elif userSelectNumber == 5:
+    print('메뉴 : 밀크티')
+    print('가격 : 4,300원')
+print('-' * 43)
