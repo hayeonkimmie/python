e3a5ce3559b375bbfcb6dad6cb104e14e0d81574
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-15 21:55:44 +0900

    condition_2_025

--- condition.py
+++ condition.py
@@ -1,41 +1,20 @@
-# passScore = 80
-# myScore = int(input('점수 입력 :'))
-# # if myScore >=passScore :
-# #     print('PASS!!')
-# #
-# # if myScore < passScore :
-# #     print('FAIL!!')
-# #
-# # if myScore >=passScore:
-# #     print('PASS!!')
-# #
-# # else:
-# #     print('FAIL!!')
-# #
-# # if myScore <passScore: #pass는 나중에 코딩하겠다라는 뜻으로 오류 안내고 보류해두려면 pass라 적어두기
-# #     pass
-# #
-# # else:
-# #     pass
-# #
-# # print(len('가나다'))
-# #
-# # if len(messageString)>=500 :
-# #     ass
-# # else:
-# #     pass
+# rainPercentage = int(input('비율 확률 입력:'))
+# minRainPercentage = 55
 #
+# print('우산 챙기세요.') if rainPercentage >= minRainPercentage else print('양산 챙기세요')
 #
-# floatNum = float(input('소수 입력:'))
-#
-# if floatNum - int(floatNum)>= 0.5:
-#     print('올림 : {}'.format(int(floatNum)+1))
+# if rainPercentage >=minRainPercentage:
+#     print('우산 챙기세요')
 # else:
-#     print('버림:{}'.format(int(floatNum)))
+#     print('양산 챙기세요')
 
-seniorAge = 65
-passengerAge =int(input('나이 입력:'))
-if passengerAge >= seniorAge:
-    print('무료 대상 승객입니다.')
+
+
+lowTemp = int(input('최저 기온 입력:'))
+highTemp = int(input('최고 기온 입력:'))
+
+tempDifference = highTemp - lowTemp
+if tempDifference>=11:
+    print('감기 조심하세요')
 else:
-    print('유료 대상 승객입니다.')
+    print('산책하기 좋은 날씨 입니다.')
