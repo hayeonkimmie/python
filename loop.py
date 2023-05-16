9833bcd1353c2fe778956ff9d0818237f1e6983a
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-16 22:39:01 +0900

    loop_2_030

--- loop.py
+++ loop.py
@@ -1,26 +1,24 @@
-# print('{} * {} = {}'.format(2,1,(2*1)))
-
-for i in range (1,10)
-    print('{} * {} = {}'.format(3,1,(3*1)))
-
-for i in range (1,10):
-    print('{} *{} = {}'.format(5,i,(5*i)))
+for i in range (10):
+    print(i)
 
 
-print('{}선수 한테 메일 발송!!'.format('박찬호'))
-print('{}선수 한테 메일 발송!!'.format('박세리'))
-print('{}선수 한테 메일 발송!!'.format('박지성'))
-print('{}선수 한테 메일 발송!!'.format('김연경'))
-print('{}선수 한테 메일 발송!!'.format('이승엽'))
+for i in range (10):
+    result = 7*i
+    print('{} * {} = {}'.format(7,i, result))
 
-players =['박찬호, 박세리, 박지성, 김연경, 이승엽']
-for player in players:
-    print('{}선수 한테 메일 발송!!'.format(player))
+for i in range(10):
+    print('{} * {} = {}'.format(7,i,(7*i)))
 
-#players에 각 개체들을 따옴표로 다 감싸주기. 그렇게 해야 리스트 내 한 객체를 하나씩으로 인정하여 구문을 반복해줌.
-players = ['박찬호', '박세리','박지성', '김연경', '이승엽']
-for player in players:
-    print('{}선수 한테 메일 발송!!'.format(player))
+for k in range(100):
+    pass #나중에 코딩 하겠다라는 의미. 실행문 당장 입력하지 않더라도, 에러안내고 다음 코드 실행시켜 줌.
 
+for h in range(5):
+    print('Hi', end = ' ')
+    print('loop statement')
 
+for num in range(5):
+    print('Hello python')
 
+for num in range(9):
+    result = gugudanNum * (num+1)
+    print('{} * {} = {}'.format(gugudanNum, (num+1), result))
