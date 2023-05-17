4525acfd2e75dca635faaa43e12c88fdc4786481
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-17 22:31:42 +0900

    loop_2_036

--- loop.py
+++ loop.py
@@ -1,32 +1,47 @@
+num=0
+
+while True :
+    print('Hello')
+    num +=1
+    if num >=5:
+        break  #반복문 종료
+print('THE END!!')
+
+sum = 0
+searchNum =0
 for i in range(100):
-    if i %7 != 0:
-        continue
-    print('{}는 7의 배수 입니다.'.format(i))
+    sum += i
 
-cnt= 0
-for i in range(100):
-    if i % 7 != 0:
-        continue
+    if sum >100:
+        searchNum = i #100 이 넘었을 때 i가 무엇인지 알고 싶은 것
+        break
 
-    print('{}는 7의 배수입니다.'.format(i))
-    cnt +=1
+print('searchNum : {}'.format(searchNum))
 
-else:
-    print('99까지의 정수 중 7의 배수는 {}개 입니다.'.format(cnt))
+# 10의 팩토리얼(10!)을 계산하는 과정에서 결과값이 50을 넘을 때의 숫자를 구하자.
+result = 1
+num=0
+for i in range(1,11):
+    result *= i
+
+    if result >50:
+        num = i
+        break
+
+print('num : {}, result :{}'.format( num, result)) #num : 5, result : 120
+
+# 새끼 강아지 체중이 2.2kg가 넘으면 이유식을 중단하려고 한다. 한번 이유식을 먹을 때 체중이 70g 증가한다고 할 때,
+# 예상되는 이유식 날짜를 구하자. (현재 체중은 800g이다.)
+limitWeight= 2200
+currentWeight=800
+date=1
+
+while True:
+    if currentWeight >=2200:
+        break
 
 
-#1부터 100까지 정수 중 3과 7의 공배수와 최소 공배수를 출력
-minNum =0
-
-for i in range (1,101):
-    if i%3 != 0  or i%7 !=0:
-        continue
-
-    print('공배수 : {}'.format(i)) #21,42,63,84
-
-    if minNum ==0:
-        minNum =i
-else:
-    print('최소 공배수 : {}'.format(minNum)) #21
-
+    date +=1
+    currentWeight +=70
+print("이유식 중단 날짜 : {}일".format(date)) #21
 
