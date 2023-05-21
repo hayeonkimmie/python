2def212db0f402868ef6f2952718a2807bfb5c98
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-21 21:11:17 +0900

    loop_2_037

--- loop.py
+++ loop.py
@@ -1,47 +1,19 @@
-num=0
-
-while True :
-    print('Hello')
-    num +=1
-    if num >=5:
-        break  #반복문 종료
-print('THE END!!')
-
-sum = 0
-searchNum =0
-for i in range(100):
-    sum += i
-
-    if sum >100:
-        searchNum = i #100 이 넘었을 때 i가 무엇인지 알고 싶은 것
-        break
-
-print('searchNum : {}'.format(searchNum))
-
-# 10의 팩토리얼(10!)을 계산하는 과정에서 결과값이 50을 넘을 때의 숫자를 구하자.
-result = 1
-num=0
-for i in range(1,11):
-    result *= i
-
-    if result >50:
-        num = i
-        break
-
-print('num : {}, result :{}'.format( num, result)) #num : 5, result : 120
-
-# 새끼 강아지 체중이 2.2kg가 넘으면 이유식을 중단하려고 한다. 한번 이유식을 먹을 때 체중이 70g 증가한다고 할 때,
-# 예상되는 이유식 날짜를 구하자. (현재 체중은 800g이다.)
-limitWeight= 2200
-currentWeight=800
-date=1
-
-while True:
-    if currentWeight >=2200:
-        break
+for i in range (1,10):
+    for j in range(i):
+        print('*', end='')
+#     print()
+#
+#
+for i in range(10,0, -1):
+    for j in range(i):
+        print('*', end='')
+    print()
 
 
-    date +=1
-    currentWeight +=70
-print("이유식 중단 날짜 : {}일".format(date)) #21
+#반복 안에 또 다른 반복이 있는 것이기 때문에 구상을 잘 해야 함.
+for i in range(1,10):
+    for j in range(2,10):
+        result = j* i
+        print('{} * {} = {} \t'.format(j, i ,result), end='')
 
+    print()
