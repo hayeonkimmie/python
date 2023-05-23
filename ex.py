a30d5ada758e361f0a82531875e66213bba25672
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-23 22:53:48 +0900

    ex_2_048

--- ex.py
+++ ex.py
@@ -1,26 +1,26 @@
-# hour = int(input('시간 입력:'))
-# min = int(input('분 입력:'))
-# sec = int(input('초 입력:'))
-# print('{}초'.format(hour*60*60+min*60+sec))
+# baseTemp  = 29
+# step = 60 #60도마다 온도가 내려감
+# stepTemp = 0.8
 #
-# # , 이용하여 가독성을 높이기 위해 format함수 재사용. , 를 사용하면 숫자가 문자 형태로 변환됨.
-# print('{}초'.format(format(hour*60*60+min*60+sec, ',')))
+# height = int(input('고도 입력:'))
+# targetTemp = baseTemp - ((height //step) *0.8)
+# if height % step != 0:
+#     targetTemp = targetTemp + stepTemp
 #
+# print('지면 온도: {}'.format(baseTemp))
+# print('고도: %dm의 기온 : %.2f도' %(height, targetTemp))
 
-money = int(input('금액 입력:'))
-rate = float(input('이율 입력:'))
-term = int(input('기간 입력:'))
+bread = 197
+milk = 152
+studentCnt = 17
 
-targetMoney = money
+print('학생 한명이 갖게되는 빵 개수 : {}'.format(bread//studentCnt))
+print('학생 한명이 갖게되는 우유 개수 : {}'.format(milk//studentCnt))
 
-for i in range(term):
-    #targetMoney=targetMoney + (targetMoney * rate * 0.01)
+print('남는 빵 개수 : {}'.format(bread %studentCnt))
 
-    targetMoney+= (targetMoney * rate * 0.01)
+print('남는 우유 개수 : {}'.format(milk %studentCnt))
 
-    #1000단위로 끊어줌
-targetMoneyFormated = format(int(targetMoney), ',')
-print('-' *30)
-print('이율:{}'.format(rate))
-print('원금:{}'.format(format(money,',')))
-print('{}년 후 금액:{}'.format(term, targetMoneyFormated))
+
+
+
