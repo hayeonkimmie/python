7b5a8de89475e8f42529d60d93b5889c034a1714
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-25 20:48:24 +0900

    ex_2_051

--- ex.py
+++ ex.py
@@ -1,35 +1,68 @@
-# speed= int(input('속도 입력:'))
-# if speed > 50:
-#     print('안전속도 위반!! 과태표 50,000원 부과 대상!!!')
-# else:
-#     print('안전속도 준수!!')
+# koreanScore=int(input('국어 점수:'))
+# engScore=int(input('영어 점수:'))
+# matScore=int(input('수학 점수:'))
+# sciScore=int(input('과학 점수:'))
+# hisScore=int(input('국사 점수:'))
+#
+# totalScore = koreanScore + engScore + matScore + sciScore + hisScore
+# avgScore = totalScore/5
+# difference =
+#
+# print('-'*50)
+#
+# print('총점 : {}({}), 평균 :{}({})'format(totalScore, avgScore))
+#
+# engScore
+# matScore
+# matScore
+# sciScore
+#
+korAvg = 85; engAvg = 82; matAvg = 89
+sciAvg = 75; hiAvg = 94
+
+totalAvg = korAvg + engAvg + matAvg + sciAvg + hiAvg
+avgAvg = int(totalAvg/ 5)
+
+korScore = int(input('국어 점수:'))
+engScore = int(input('영어 점수:'))
+matScore = int(input('수학 점수:'))
+sciScore = int(input('과학 점수:'))
+hiScore = int(input('국사 점수:'))
 
 
-# inputMsg = input('메시지 입력:')
-# lengthMsg = len(inputMsg)
-# if len(inputMsg) <=50:
-#     print('SMS 발송!!')
-#     print('메시지 길이 : {}'.format(lengthMsg))
-#     print('메시지 발송 요금: 50원')
-# else:
-#     print('MMS 발송!!')
-#     print('메시지 길이 : {}'.format(lengthMsg))
-#     print('메시지 발송 요금: 100원')
+totalScore = korScore + engScore + matScore + sciScore + hiScore
+avgScore = int(totalScore/5)
 
+korGap = korScore - korAvg
+engGap = engScore - engAvg
+matGap = matScore - matAvg
+sciGap = sciScore - sciAvg
+hiGap = hiScore - hiAvg
 
-message = input('메시지 입력:')
-lenMessage = len(message)
-msgPrice = 50
+totalGap = totalScore - totalAvg
+avgGap = avgScore - avgAvg
 
-if lenMessage <= 50:
-    msgPrice = 50
-    print('SMS 발송!!')
+print('-' * 70)
+print('총점 : {}({}), 평균 : {}({})'.format(totalScore, totalGap, avgScore, avgGap))
+print('국어 : {}({}), 영어 : {}({}), 수학 : {}({}), 과학 : {}({}), 국사: {}({})'.format(
+    korScore, korGap, engScore, engGap, matScore, matGap, sciScore, sciGap, hiScore, hiGap))
 
-if lenMessage >50:
-    msgPrice = 100
-    print('MMS 발송!!')
+print('-' * 70)
 
-print('메시지 길이 : {}'.format(lenMessage))
-print('메시지 발송 요금: {}'.format(msgPrice))
+str = '*' if korGap > 0 else '-'
+print('국어 편차 : {}({})'.format(str * abs(korGap), korGap))  # abs : 절대값 구하는 함수. - 수를 곱하면 0이기 때문
+str = '*' if engGap > 0 else '-'
+print('영어 편차 : {}({})'.format(str * abs(engGap), engGap))  # abs : 절대값 구하는 함수. - 수를 곱하면 0이기 때문
+str = '*' if matGap > 0 else '-'
+print('수학 편차 : {}({})'.format(str * abs(matGap), matGap))  # abs : 절대값 구하는 함수. - 수를 곱하면 0이기 때문
+str = '*' if sciGap > 0 else '-'
+print('과학 편차 : {}({})'.format(str * abs(sciGap), sciGap))  # abs : 절대값 구하는 함수. - 수를 곱하면 0이기 때문
+str = '*' if hiGap > 0 else '-'
+print('국사 편차 : {}({})'.format(str * abs(hiGap), hiGap))  # abs : 절대값 구하는 함수. - 수를 곱하면 0이기 때문
 
-ㄷ
+str = '*' if totalGap >0 else '-'
+print('총점 편차 : {}({})'.format(str * abs(totalGap), totalGap))
+
+str = '*' if avgGap >0 else '-'
+print('평균 편차 : {}({})'.format(str * abs(avgGap), avgGap))
+
