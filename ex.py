87fcbeb090df59791162306f5c89b2d8e7ef865b
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-26 21:24:18 +0900

    ex_2_055

--- ex.py
+++ ex.py
@@ -1,38 +1,40 @@
-import datetime
-today = datetime.datetime.today() #날짜, 실시간 시간 다 나옴. 2023-03-06 21:46:42.527218
+# import random
+# rNum = random.randint(1,1000)
+# tryCount = 0
+#
+#
+# #반복문을 통해 계속 물어보아야 함
+# gameFlag = True
+# while gameFlag :
+#     tryCount +=1
+#     pNum = int(input('1에서 1000까지의 정수 입력:')) #난수를 맞춰보세요
+#
+#     if rNum == pNum :
+#         print('빙고!')
+#         gameFlag = False
+#     else:
+#         if rNum > pNum :
+#             print('난수가 크다!')
+#         else:
+#             print('난수가 작다!')
+#
+# print('난수 : {}, 시도 횟수: {}'.format(rNum, tryCount))
+# 다자 택일문
 
-day = today.day #날짜만 뽑아낼 수 있음.
-limitDust = 150
-dustNum = int(input('미세먼지 수치 입력: ')) #실행문 숫자 비교를 위해, int로 형변환 해주기
-carType = int(input('차량 종류 선택: 1. 승용차 2. 영업용차'))
-carNumber = int(input('차량 번호 입력:'))
 
-print('-' * 30)
-print(today)
-print('-' * 30)
-# 나중에 실행문 코딩하고 싶을 때는 pass입력해두고 일단 넘어가기
-# if dustNum >= limitDust and carType ==1:
-#     pass
-# if dustNum >= limitDust and carType ==2:
-#     pass
 
-if dustNum >limitDust and carType ==1:
-    if (day %2) == (carNumber %2):
-        print('차량 2부제 적용')
-        print('차량 2부제로 금일 운행제한 대상 차량입니다.')
-    else:
-        print('금일 운행 가능합니다.')
+innerTemp = int(input('실내 온도 입력:'))
+if innerTemp <= 18:
+    print('에어컨 : OFF!!')
+elif innerTemp >18 and innerTemp <=22:
+    print('에어컨 : 매우 약!!')
+elif innerTemp >22 and innerTemp <=24:
+    print('에어컨 : 약!!')
+elif innerTemp >24 and innerTemp <=26:
+    print('에어컨 : 중!!')
+elif innerTemp >26 and innerTemp <=28:
+    print('에어컨 : 강!!')
+elif innerTemp >28:
+    print('에어컨 : 매우 강!!')
 
-if dustNum > limitDust and carType ==2:
-    if (day %5) == (carNumber %5):
-        print('차량 5부제 적용')
-        print('차량 5부제로 금일 운행제한 대상 차량입니다.')
-    else:
-        print('금일 운행 가능합니다.')
 
-if dustNum <=limitDust :
-    if (day %5) == (carNumber %5):
-        print('차량 5부제 적용')
-        print('차량 5부제로 금일 운행제한 대상 차량입니다.')
-    else:
-        print('금일 운행 가능합니다.')
