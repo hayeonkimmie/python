69e177486f1d436467516ef500bbbde73075b236
hayeonkimmie <124011034+hayeonkimmie@users.noreply.github.com>
2023-05-26 21:23:52 +0900

    ex_2_054

--- ex.py
+++ ex.py
@@ -1,29 +1,38 @@
-part = int(input('업종 선택 (1. 가정용 \t 2. 대중탕용 \t 3. 공업용) :'))
-useWater = int(input('사용량 입력:'))
-unitPrice = 0 #업종에 따라서 단가 달라지기 때문에 일단 0으로 설정
+import datetime
+today = datetime.datetime.today() #날짜, 실시간 시간 다 나옴. 2023-03-06 21:46:42.527218
 
-if part ==1:
-    unitPrice = 540
-elif part ==2:
-    if useWater <= 50:
-        unitPrice = 820
-    elif useWater > 50 and useWater <= 300:
-        unitPrice = 1920
-    elif useWater > 300 :
-        unitPrice = 2400
-    pass
+day = today.day #날짜만 뽑아낼 수 있음.
+limitDust = 150
+dustNum = int(input('미세먼지 수치 입력: ')) #실행문 숫자 비교를 위해, int로 형변환 해주기
+carType = int(input('차량 종류 선택: 1. 승용차 2. 영업용차'))
+carNumber = int(input('차량 번호 입력:'))
 
-elif part == 3:
-    if useWater <=500:
-        unitPrice = 240
-    else:
-        unitPrice = 470
-
-print('*' *30)
-print('상수도 요금표')
 print('-' * 30)
-print('사용량 \t : \t 요금')
+print(today)
+print('-' * 30)
+# 나중에 실행문 코딩하고 싶을 때는 pass입력해두고 일단 넘어가기
+# if dustNum >= limitDust and carType ==1:
+#     pass
+# if dustNum >= limitDust and carType ==2:
+#     pass
 
-userPrice = useWater * unitPrice
-print('{} \t : \t {}원'.format(useWater, userPrice))
-print('=' *50)
+if dustNum >limitDust and carType ==1:
+    if (day %2) == (carNumber %2):
+        print('차량 2부제 적용')
+        print('차량 2부제로 금일 운행제한 대상 차량입니다.')
+    else:
+        print('금일 운행 가능합니다.')
+
+if dustNum > limitDust and carType ==2:
+    if (day %5) == (carNumber %5):
+        print('차량 5부제 적용')
+        print('차량 5부제로 금일 운행제한 대상 차량입니다.')
+    else:
+        print('금일 운행 가능합니다.')
+
+if dustNum <=limitDust :
+    if (day %5) == (carNumber %5):
+        print('차량 5부제 적용')
+        print('차량 5부제로 금일 운행제한 대상 차량입니다.')
+    else:
+        print('금일 운행 가능합니다.')
